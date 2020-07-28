25984a270316c87c855b955e038d05746d9f24ff
cmx <2790673868@qq.com>
2020-07-28 16:28:27 +0800

    终于能提交了

--- src/main/java/org/sacc/interact/InteractApplication.java
+++ src/main/java/org/sacc/interact/InteractApplication.java
@@ -1,9 +1,11 @@
 package org.sacc.interact;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "org.sacc.interact.mapper")
 public class InteractApplication {
 
     public static void main(String[] args) {
